32e4fbe91b64a8b77405a129175a8a19883d66cf
Miguel Lourenço <mse.lourenco@campus.fct.unl.pt>
2025-04-12 00:09:00 +0100

    Change Role works correctly (Demonstrable).

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ChangeResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ChangeResource.java
@@ -18,6 +18,80 @@ public class ChangeResource {
     public ChangeResource() {}
 
     @POST
+    @Path("/role")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response changeRole(ChangeRoleData data) {
+
+        if(data.changer == null || data.user == null || data.old_role == null || data.new_role == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Please provide a changer, user, old role and new role.")
+                    .build();
+        }
+
+        Key changerKey = userKeyFactory.newKey(data.changer);
+        Entity changer = datastore.get(changerKey);
+
+        if(changer == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Changer not found.")
+                    .build();
+        }
+
+        Key userKey = userKeyFactory.newKey(data.user);
+        Entity user = datastore.get(userKey);
+
+        if(user == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("User not found.")
+                    .build();
+        }
+
+        boolean backoffice = data.changer.equals("backoffice") && !((data.old_role.equals("enduser") && data.new_role.equals("partner")) ||
+                                                                    (data.old_role.equals("partner") && data.new_role.equals("enduser")));
+        boolean enduser = data.changer.equals("enduser");
+
+        if(backoffice || enduser) {
+            return Response.status(Response.Status.FORBIDDEN)
+                    .entity("Changer does not have permission to change the user's role.")
+                    .build();
+        }
+
+        if(!data.old_role.equals(user.getString("user_role"))) {
+            return Response.status(Response.Status.FORBIDDEN)
+                    .entity("Please enter the correct old role.")
+                    .build();
+        }
+
+        if(data.old_role.equals(data.new_role)) {
+            return Response.status(Response.Status.FORBIDDEN)
+                    .entity("Please enter a different new role.")
+                    .build();
+        }
+
+        Entity updatedUser = Entity.newBuilder(user)
+                .set("user_role", data.new_role)
+                .build();
+
+        datastore.update(updatedUser);
+
+        return Response.ok().entity("Role changed successfully.").build();
+    }
+
+    @POST
+    @Path("/state")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response changeState(ChangeStateData data) {
+        return null;
+    }
+
+    @POST
+    @Path("/attributes")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response changeAttributes(ChangeAttributesData data) {
+        return null;
+    }
+
+    @POST
     @Path("/password")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response changePassword(ChangePasswordData data) {
@@ -57,26 +131,5 @@ public class ChangeResource {
         datastore.update(updatedUser);
 
         return Response.ok().entity("Password changed successfully.").build();
-    }
-
-    @POST
-    @Path("/role")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response changeRole(ChangeRoleData data) {
-        return null;
-    }
-
-    @POST
-    @Path("/attributes")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response changeAttributes(ChangeAttributesData data) {
-        return null;
-    }
-
-    @POST
-    @Path("/state")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response changeState(ChangeStateData data) {
-        return null;
     }
 }
